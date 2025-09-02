f992ad75caa714900fa0853d832347ba43dcfe79
Commander-Ayush <ayushkrjha221004@gmail.com>
2025-09-02 07:31:41 +0530

    all the blocks+coins+ghosts have been counted

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const boardWidth = columnCount*tileSize;
 const boardHeight = rowCount * tileSize;
 let context;
 
-
 //images
 let blueGhostImage;
 let greenGhostImage;
@@ -24,13 +23,20 @@ let cherry;
 let cherry2;
 
 
+//<---------------onload Function--------------->
 window.onload = function(){
     board=document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
+    loadMap();
+    console.log(walls.size);
+    console.log(coins.size);
+    console.log(ghosts.size);
 }
 
+
+//<-----------------Image Loader----------------->
 function loadImage(){
     wallImage = new Image();
     wallImage.src="./wall.png";
@@ -61,4 +67,93 @@ function loadImage(){
 
     pacmanRightImage = new Image();
     pacmanRightImage.src = "./pacmanRight";
+}
+
+//<--------------------Tile Map--------------------->
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX"
+];
+
+const walls = new Set();
+const ghosts = new Set();
+const coins  = new Set();
+let pacman;
+
+function loadMap(){
+    walls.clear();
+    coins.clear();
+    ghosts.clear();
+
+    for(let row = 0; row<rowCount; row++){
+        for(let columnElement = 0; columnElement<columnCount;columnElement++){
+            const rows = tileMap[row];
+            const tileMapChar = rows[columnElement];
+
+            const xPosition = columnElement*tileSize;
+            const yPosition = row*tileSize;
+
+            if(tileMapChar == 'X'){ //block wall
+                const wall = new Block(wallImage, xPosition, yPosition, tileSize, tileSize);
+                walls.add(wall);
+            }
+            else if(tileMapChar == 'b'){ //blue Ghost
+                const ghost = new Block(blueGhostImage, xPosition, yPosition, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if(tileMapChar == 'o'){ //orange Ghost
+                const ghost = new Block(orangeGhostImage, xPosition, yPosition, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if(tileMapChar == 'p'){ //pink Ghost
+                const ghost = new Block(pinkGhostImage, xPosition, yPosition, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if(tileMapChar == 'r'){ //red Ghost
+                const ghost = new Block(redGhostImage, xPosition, yPosition, tileSize, tileSize);
+                ghosts.add(ghost);
+            }
+            else if(tileMapChar == "P"){ //Pacman
+                pacman = new Block(pacmanRightImage, xPosition, yPosition, tileSize, tileSize);
+            }
+            else if(tileMapChar == " "){ //Coins
+                const coin = new Block(null, xPosition+14, yPosition+14, 4, 4);
+                coins.add(coin);
+            }
+        }
+    }
+}
+
+//<-----------------------Blocks------------------------------->
+
+class Block{
+    constructor(image, x, y, width, height){
+        this.image = image;
+        this.xCoordinate=x;
+        this.yCoordinate=y;
+        this.blockWidth=width;
+        this.blockHeight=height;
+
+        this.startX = x;
+        this.startY = y;
+    }
 }
